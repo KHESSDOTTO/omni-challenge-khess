f3d960adcbcad14f5fbfb3a5743b02b890c7ae06
Khess Santomauro Dotto <khess@injoy.ag>
2025-02-22 21:08:35 -0300

    Added env variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // Variáveis de ambiente
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
