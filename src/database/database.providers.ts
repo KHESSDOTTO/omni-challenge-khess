e47ff1ec633752a0d5a4d6bf6d1434905c882a02
Khess Santomauro Dotto <khess@injoy.ag>
2025-02-24 17:09:00 -0300

    Added database url for connection on render after deployment.

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,26 +1,41 @@
 import { DataSource } from 'typeorm';
 
-type dbType = 'postgres' | 'mysql';
-
 export const databaseProviders = [
   // Main provider
   {
     provide: 'DATA_SOURCE',
     useFactory: async () => {
-      const dataSource = new DataSource({
-        type: process.env.DB_TYPE as dbType,
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE_NAME,
-        entities: [
+      if (process.env.DATABASE_URL) {
+        const dataSource = new DataSource({
+          type: 'postgres',
+          url: process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false // Required for Render Postgres
+          },
+          entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
-        ],
-        synchronize: true, // Alterar no futuro (produção) - feito somente para demonstração
-      });
+          ],
+          synchronize: true, // Change to false and use migrations in production
+        });
+        
+        return dataSource.initialize();
+      } else {
+        const dataSource = new DataSource({
+          type: process.env.DB_TYPE as 'postgres' | 'mysql',
+          host: process.env.DB_HOST,
+          port: Number(process.env.DB_PORT),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE_NAME,
+          entities: [
+              __dirname + '/../**/*.entity{.ts,.js}',
+          ],
+          synchronize: true, // Alterar no futuro (produção) - feito somente para demonstração
+        });
 
-      return dataSource.initialize();
+        return dataSource.initialize();
+      }
+
     },
   },
 ];
